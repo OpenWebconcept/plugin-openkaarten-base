cd0dbfaabd5b33215212ff2f951ec82d8e5c716f
Eyal Beker <eyal@acato.nl>
2024-11-04 13:28:28 +0100

    Removed unused code

--- src/scripts/openstreetmap.js
+++ src/scripts/openstreetmap.js
@@ -2,7 +2,7 @@ import L from "leaflet";
 import { MarkerClusterGroup } from "leaflet.markercluster/src";
 
 // Retrieve the locations and map configuration from the global window object.
-const { mapLocations, minLat, maxLat, minLong, maxLong, centerLat, centerLong, defaultZoom, fitBounds, allowClick } = window.leaflet_vars;
+const { mapLocations, minLat, maxLat, minLong, maxLong, centerLat, centerLong, defaultZoom, fitBounds } = window.leaflet_vars;
 const locationItems = mapLocations ? JSON.parse(mapLocations) : [];
 
 // Set the map configuration.
@@ -49,64 +49,6 @@ if ( fitBounds ) {
 L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-if ( allowClick ) {
-  map.on( 'click', function (e) {
-    var coord = e.latlng;
-    var lat = coord.lat;
-    var lng = coord.lng;
-
-    // Remove the existing markers.
-    map.eachLayer( function (layer) {
-      if (layer instanceof L.Marker) {
-        map.removeLayer( layer );
-      }
-    } );
-
-    // Add a draggable marker to the map.
-    // Create a custom marker icon with the location color and icon.
-    let customIconHtml = "<div style='background-color:" + location.color + ";' class='marker-pin'></div>";
-    if (location.icon) {
-      customIconHtml += "<span class='marker-icon'><img src='" + location.icon + "'  alt='marker icon' /></span>";
-    }
-
-    var customIcon = L.divIcon( {
-      className: 'leaflet-custom-icon',
-      html: customIconHtml,
-      iconSize: [30, 42],
-      iconAnchor: [15, 42]
-    } );
-
-    let iconOptions = {
-      icon: customIcon,
-      draggable: 'true'
-    }
-
-    var marker = L.marker( [lat, lng], iconOptions );
-    marker.on( 'dragend', function (event) {
-      var marker = event.target;
-      var position = marker.getLatLng();
-      marker.setLatLng( new L.LatLng( position.lat, position.lng ), {draggable: 'true'} );
-      map.panTo( new L.LatLng( position.lat, position.lng ) );
-
-      // Update the form fields with the new coordinates.
-      updateGeoFields( position.lat, position.lng );
-    } );
-    map.addLayer( marker );
-
-    // Update the form fields with the new coordinates.
-    updateGeoFields( lat, lng );
-  } );
-}
-
-function updateGeoFields(lat, lng) {
-  if ( document.getElementById('field_geo_latitude') !== null ) {
-    document.getElementById( 'field_geo_latitude' ).value = lat;
-  }
-  if ( document.getElementById('field_geo_longitude') !== null ) {
-    document.getElementById( 'field_geo_longitude' ).value = lng;
-  }
-}
 
 // Add locations to the map as markers.
 if ( locationItems.length !== 0 ) {
